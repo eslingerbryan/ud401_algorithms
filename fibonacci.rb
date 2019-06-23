aad4e03db314e73bd24e1595b2478d65d22cc0f7
Bryan Eslinger <eslinger.bryan@gmail.com>
2019-06-23 12:34:39 -0700

    Implement dynamic programming fibonacci algorithm

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 
 class TestFibonacci < Minitest::Test
+  FIB_500 = 139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125
+
   def test_recursive_base_case
     fibonacci = Fibonacci.new
     assert_equal(1, fibonacci.recursive(1))
@@ -10,6 +12,21 @@ class TestFibonacci < Minitest::Test
     fibonacci = Fibonacci.new
     assert_equal(21, fibonacci.recursive(8))
   end
+
+  def test_dynamic_base
+    fibonacci = Fibonacci.new
+    assert_equal(1, fibonacci.dynamic(1))
+  end
+
+  def test_dynamic
+    fibonacci = Fibonacci.new
+    assert_equal(21, fibonacci.dynamic(8))
+  end
+
+  def test_dynamic_large_n
+    fibonacci = Fibonacci.new
+    assert_equal(FIB_500, fibonacci.dynamic(500))
+  end
 end
 
 class Fibonacci
@@ -18,5 +35,12 @@ class Fibonacci
     return 1 if n == 1
 
     return recursive(n - 2) + recursive(n - 1)
+  end
+
+  def dynamic(n)
+    fibonaccis = [0, 1]
+    (2..n).each { |i| fibonaccis << fibonaccis[i - 2] + fibonaccis[i - 1] }
+
+    fibonaccis[n]
   end
 end
